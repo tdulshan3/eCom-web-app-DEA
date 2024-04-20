ffcd0c36db9298a8a8ad13c47527ebb0469b4b6b
Adhishtanaka Kulasooriya <154827022+Adhishtanaka@users.noreply.github.com>
2024-04-21 00:14:08 +0530

    fixing merge conflict issues

--- eCom-web-app-DEA/src/java/adminpackage/CancelOrderServlet.java
+++ eCom-web-app-DEA/src/java/adminpackage/CancelOrderServlet.java
@@ -6,12 +6,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/CancelOrderServlet")
 public class CancelOrderServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
--- eCom-web-app-DEA/src/java/adminpackage/OrdersServlet.java
+++ eCom-web-app-DEA/src/java/adminpackage/OrdersServlet.java
@@ -6,12 +6,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/OrdersServlet")
 public class OrdersServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
