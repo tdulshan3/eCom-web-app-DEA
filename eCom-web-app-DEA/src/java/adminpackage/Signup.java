e78121d67a9c51550db2b19a17f47d8b75dc8d00
TanujMalinda <111776904+TanujMalinda@users.noreply.github.com>
2024-04-29 19:31:45 +0530

    Update comments

--- eCom-web-app-DEA/src/java/adminpackage/Signup.java
+++ eCom-web-app-DEA/src/java/adminpackage/Signup.java
@@ -16,7 +16,7 @@ public class Signup extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-
+//getting parameters from login&signup.jsp 
         String email = request.getParameter("email");
         String pw1 = request.getParameter("password1");
         String pw2 = request.getParameter("password2");
@@ -26,38 +26,42 @@ public class Signup extends HttpServlet {
         String townCity = request.getParameter("towncity");
         String postcode = request.getParameter("postcode");
         String phoneNumber = request.getParameter("pnumber");
-
+//check both password and confirm password are equal or not
         if (!pw1.equals(pw2)) {
             request.setAttribute("errorMessage1", "Passwords do not match");
             request.setAttribute("formParam", "signup");
             RequestDispatcher dispatcher = request.getRequestDispatcher("/login&signup.jsp");
             dispatcher.forward(request, response);
             return;
+        
         } else {
-
+//if both password's are equal this execute
             try {
                 Dbcon dbConnector = new Dbcon();
                 dbConnector.connect();
-
+//check user already have an account
                 if (checkEmailExists(dbConnector, email)) {
                     String status = getEmailStatus(dbConnector, email);
+                    //if user account is already exist check it status is registered or unregistered
                     if ("registered".equals(status)) {
+                        //if user is already registered then send an error massage 
                         request.setAttribute("formParam", "signup");
                         request.setAttribute("errorMessage1", "Email already exists");
                         RequestDispatcher dispatcher = request.getRequestDispatcher("/login&signup.jsp");
                         dispatcher.forward(request, response);
                         return;
                     } else {
+                        //if user is unregistered this will update his details and chenge status to registered
                         String query1 = "UPDATE user SET Password = ?, FirstName = ?, LastName = ?, Address = ?, City = ?, Postcode = ?, Phone = ?, status = ? WHERE email = ?";
                         dbConnector.executePreparedStatement(query1, pw1, firstName, lastName, address, townCity, postcode, phoneNumber, "registered", email);
                     }
 
                 } else {
-
+                //if user detail not available in user table this will create a record about user
                     String query = "INSERT INTO user (email, Password, FirstName, LastName, Address, City, Postcode, Phone, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
                     dbConnector.executePreparedStatement(query, email, pw1, firstName, lastName, address, townCity, postcode, phoneNumber, "registered");
                 }
-
+//this will redirect to login&signup.jsp
                 response.sendRedirect(request.getContextPath() + "/login&signup.jsp");
 
             } catch (ClassNotFoundException | SQLException ex) {
@@ -65,12 +69,12 @@ public class Signup extends HttpServlet {
             }
         }
     }
-
+//this function will check email is exist on user table or not by using email as a parameter 
     private boolean checkEmailExists(Dbcon dbConnector, String email) throws SQLException {
         ResultSet rs = dbConnector.executeQueryWithPreparedStatement("SELECT * FROM user WHERE email = ?", email);
         return rs.next();
     }
-
+//this function will check, status of user in user table using email as a parameter
     private String getEmailStatus(Dbcon dbConnector, String email) throws SQLException {
         ResultSet rs = dbConnector.executeQueryWithPreparedStatement("SELECT status FROM user WHERE email = ?", email);
         if (rs.next()) {
